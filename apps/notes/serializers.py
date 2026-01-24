aeb33539b3cb41fe7804cb7beec3e1d94813dcf8
foxDev01 <faraday073@gmail.com>
2026-01-24 06:51:26 +0000

    работа над Views

--- apps/notes/serializers.py
+++ apps/notes/serializers.py
@@ -4,3 +4,29 @@ from rest_framework import serializers
 from gjango.utils.html import escape
 from .models import Note
 
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Note
+        field = ['id','title','content','created_at','update_at']
+        read_only_field=['id','created_at','update_at']
+# проверка наличия заголовка
+    def validate_title(self,value):
+        if not value.strip():
+            raise serializers.ValidateError('Заголовок не может быть пустым')
+        return escape(value.strip())
+# проверка наличия контента
+    def validate_content(self,value):
+        if not value.strip():
+            raise serializers.ValidateError('Содержание не может быть пустым')
+        return escape(value.strip())
+
+
+class NoteCreateSerializer(NoteSerializer):
+    pass
+
+
+class NoteUpdateSerializer(NoteSerializer):
+    # для обновлвения заметок делает поля необязательными 
+    title = serializers.CharField(required=False)
+    content = serializers.CharField(required=False)
--- apps/notes/views.py
+++ apps/notes/views.py
@@ -1,3 +1,35 @@
+# содержит представления для обработки запросов апи 
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+from .models import Note
+from .serializers import NoteCreateSerializer, NoteSerializer, NoteUpdateSerializer
+
+
+class NoteListCreateViews(generics.ListAPIView):
+    queryset = None.objects.all()
+
+    def get_serializer_class(self):
+ 
+        return NoteSerializer
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == ["POST", "PATCH"]:
+            return NoteCreateSerializer
+        return NoteSerializer
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', false)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+
+        if serializer.is_valid():
+            self.perform_update(serializer)
